d6040dc4974482280495bacc67b18b35182b25e1
Rodrigo <rodrigorom2000@gmail.com>
2023-05-06 19:51:20 -0400

    changed epochs

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ from utils import (
 LEARNING_RATE = 1e-3
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 16
-NUM_EPOCHS = 1000
+NUM_EPOCHS = 200
 NUM_WORKERS = 2
 IMAGE_HEIGHT = 256
 IMAGE_WIDTH = 256
